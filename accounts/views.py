1d85178497ba46a4617a0cf285d90feb22f10137
Arjwanaldrbas <443202358@student.ksu.edu.sa>
2026-03-18 20:23:08 +0300

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,48 +2,38 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import RegisterForm
-# Create your views here.
 
 def index(request):
-
     return redirect('login')
 
 def signUp(request):
-
     if request.method == "POST":
         form = RegisterForm(request.POST)
-
         if form.is_valid():
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password'])
             user.save()
-
-            messages.success(request," The account created successfully !!")
+            messages.success(request, "The account was created successfully! Please login.")
             return redirect('login')
-
     else:
         form = RegisterForm()
 
-    return render(request,'signUp.html',{'form':form})
+    return render(request, 'signup.html', {'form': form})
 
 def loginV(request):
-
     if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-        username = request.POST['username']
-        password = request.POST['password']
-
-        u= authenticate(request,username=username,password=password)
+        u = authenticate(request, username=username, password=password)
 
         if u is not None:
-            login(request,u)
-            return redirect('movies')
-
+            login(request, u)
+            return redirect('movies_list')
         else:
-            messages.error(request,"Incorect username or password try agin ")
+            messages.error(request, "Incorrect username or password. Please try again.")
 
-    return render(request,'login.html')
-
+    return render(request, 'login.html')
 
 def logoutV(request):
     logout(request)
