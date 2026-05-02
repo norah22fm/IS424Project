2dd4bca16390d50ad96c44c13907a865310e7374
Arjwanaldrbas <443202358@student.ksu.edu.sa>
2026-05-02 17:58:23 +0300

    Update views.py

--- IS424Project-main/IS424Project-main/movies/views.py
+++ IS424Project-main/IS424Project-main/movies/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
 from .models import Movie, Rating
 from django.db.models import Avg
-from django.contrib import messages
 from .forms import MovieForm
 from django.contrib.auth.decorators import login_required
 
@@ -9,64 +8,109 @@ def movie_list(request):
     movies = Movie.objects.annotate(average_rating=Avg('rating__rating'))
     return render(request, 'movies.html', {'movies': movies})
 
+
 def movie_detail(request, movie_id):
-    movie = get_object_or_404(Movie, id=movie_id)
+    movie = Movie.objects.get(id=movie_id)
+
+    ratings = Rating.objects.filter(movie=movie)
 
     avg_data = Rating.objects.filter(movie=movie).aggregate(Avg('rating'))
     average_rating = avg_data['rating__avg'] or 0
-
     user_rating = None
+    message = ""
+
     if request.user.is_authenticated:
         user_rating = Rating.objects.filter(movie=movie, user=request.user).first()
 
     if request.method == "POST":
         if not request.user.is_authenticated:
-            messages.info(request, "Please login to rate movies.")
             return redirect('login')
 
         rating_value = request.POST.get("rating")
+
         if user_rating:
             user_rating.rating = rating_value
             user_rating.save()
-            messages.success(request, "Your rating has been updated!")
+            message = "Your rating has been updated!"
         else:
             Rating.objects.create(user=request.user, movie=movie, rating=rating_value)
-            messages.success(request, "Rating submitted successfully!")
-        return redirect('movie_detail', movie_id=movie.id)
+            message = "Rating submitted successfully!"
 
     return render(request, 'movie_detail.html', {
-        'movie': movie,
-        'average_rating': round(average_rating, 1),
-        'user_rating': user_rating
-    })
+    'movie': movie,
+    'average_rating': round(average_rating, 1),
+    'user_rating': user_rating,
+    'ratings': ratings,
+    'message': message
+})
+    
+
 
 @login_required
 def add_movie(request):
+    message = ""
+
     if request.method == "POST":
-        form = MovieForm(request.POST)
+        form = MovieForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('movies_list')
+            message = "Movie added successfully!"
+            return render(request, 'movie_form.html', {
+                'form': MovieForm(),
+                'title': 'Add New Movie',
+                'message': message
+            })
     else:
         form = MovieForm()
-    return render(request, 'movie_form.html', {'form': form, 'title': 'Add New Movie'})
+
+    return render(request, 'movie_form.html', {
+        'form': form,
+        'title': 'Add New Movie',
+        'message': message
+    })
+
 
 @login_required
 def update_movie(request, movie_id):
-    movie = get_object_or_404(Movie, id=movie_id)
+    movie = Movie.objects.get(id=movie_id)
+    message = ""
+
     if request.method == "POST":
-        form = MovieForm(request.POST, instance=movie)
+        form = MovieForm(request.POST, request.FILES, instance=movie)
         if form.is_valid():
             form.save()
-            return redirect('movie_detail', movie_id=movie.id)
+            message = "Movie updated successfully!"
+            return render(request, 'movie_form.html', {
+                'form': form,
+                'title': 'Update Movie',
+                'message': message
+            })
     else:
         form = MovieForm(instance=movie)
-    return render(request, 'movie_form.html', {'form': form, 'title': 'Update Movie'})
+
+    return render(request, 'movie_form.html', {
+        'form': form,
+        'title': 'Update Movie',
+        'message': message
+    })
+
 
 @login_required
 def delete_movie(request, movie_id):
-    movie = get_object_or_404(Movie, id=movie_id)
+    movie = Movie.objects.get(id=movie_id)
+
     if request.method == "POST":
         movie.delete()
-        return redirect('movies_list')
+        movies = Movie.objects.all()
+        return render(request, 'movies.html', {
+            'movies': movies,
+            'message': 'Movie deleted successfully!'
+        })
+
     return render(request, 'movie_delete.html', {'movie': movie})
+
+
+@login_required
+def my_ratings(request):
+    ratings = Rating.objects.filter(user=request.user)
+    return render(request, 'my_ratings.html', {'ratings': ratings})
