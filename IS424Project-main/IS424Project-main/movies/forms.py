a48e4e0b38050cd717e731363813dff497a28dad
Arjwanaldrbas <443202358@student.ksu.edu.sa>
2026-05-02 17:56:54 +0300

    Update forms.py

--- IS424Project-main/IS424Project-main/movies/forms.py
+++ IS424Project-main/IS424Project-main/movies/forms.py
@@ -4,7 +4,6 @@ from .models import Movie
 class MovieForm(forms.ModelForm):
     class Meta:
         model = Movie
-        fields = ['title', 'genre', 'release_year', 'description']
-        widgets = {
+        fields = ['title', 'genre', 'release_year', 'description', 'image']        widgets = {
             'description': forms.Textarea(attrs={'rows': 4}),
         }
